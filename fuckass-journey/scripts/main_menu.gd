e1c0e152660eb427c6fa2a408f86cb8c97fd6bc1
Logan Culliton <loganculliton@gmail.com>
2025-10-18 15:51:07 -0700

    Cleaned up TileMap atlas textures to fix 230 debug errors (yikes)

--- fuckass-journey/scripts/main_menu.gd
+++ fuckass-journey/scripts/main_menu.gd
@@ -1,19 +1,9 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+# Handles starting the game and loading the first level
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/levelOne.tscn")
 
-
+# Quits out of application
 func _on_quit_pressed() -> void:
 	get_tree().quit()
