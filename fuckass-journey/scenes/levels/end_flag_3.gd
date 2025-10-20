e004862f301295958b7fb93c6c225fc47f96fcb4
Logan Culliton <loganculliton@gmail.com>
2025-10-20 01:10:17 -0700

    Added end level flag for level 3

---
+++ fuckass-journey/scenes/levels/end_flag_3.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+func _on_end_flag_3_body_entered(body: Node2D) -> void:
+	# Checks if the player entered the flag, if so move on to next level
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file("res://scenes/cutscenes/credits.tscn")
